dac49f430c99a30f336bcfe3fcce1bd20f4adbac
Md Jamaluddin <mdjamaluddina007@gmail.com>
2025-03-20 14:44:49 +0530

    second commit

--- portfolio-main/src/components/Home.jsx
+++ portfolio-main/src/components/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
           <p className="italic mb-6">I thrive on turning ideas into real-world solutions.</p>
           <div className="flex justify-center md:justify-start gap-4">
             <a
-              href="./Resume(4).pdf"
+              href="./resume.pdf"
               className="bg-yellow-400 text-black py-2 px-4 rounded hover:bg-yellow-300 transition"
               download
             >
